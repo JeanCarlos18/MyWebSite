873442dd09b8acc62f798e6fbefa220f117a4b2e
JeanVillarroel <villarroelcumanajc@gmail.com>
2023-06-20 18:48:41 -0300

    form contacto impreme value en consola

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,87 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+
 export default function Home() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>My WebSite </title>
+        <link rel="icon" href="/favi" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello Welcome to Portafolio <a href="https://www.instagram.com/"> :)</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Quiero contarles quien soy , pero como hago eso ? <code></code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.linkedin.com/in/jean-villarroel-b57590219/" className={styles.card}>
+            <h3>linkedin &rarr;</h3>
+            <p>En mi linkedin muestro mi intencion de conectar con personas que me sumen y poder sumarles</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://github.com/JeanCarlos18" className={styles.card}>
+            <h3>GIT&rarr;</h3>
+            <p>En git podran ver lo que puedo hacer de manera practica , podran explorar mis distintos proyectos y habilidades</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://www.instagram.com/jeanki0318/"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3> Fotografia &rarr;</h3>
+            <p>Siempre que salgo en bicicleta me gusta capturar recuerdo o momentos en foto siento que habla de mi y lo comparto en mi Instagram donde muestro eso momentos</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Proyectos&rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Esta seccion sera para publicar los trabajos Freelance que tenga
             </p>
           </a>
         </div>
+        <h1 className={styles.title}>Contactame
+        </h1>
+
+        <form className={styles.contacto}>
+
+          <div>
+            <label>Nombre:</label>
+            <input
+              type="text"
+              id="name"
+              onChange={handleSubmit}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              onChange={handleSubmit}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Mensaje:</label>
+            <input className='texto-message' type='text' onChange={handleSubmit}>
+
+            </input>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
       </main>
 
       <footer>
